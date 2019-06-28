af9934e9e55c3e23f531597ea97b943dce61165e
ahodesuyo <52289471+ahodesuyo@users.noreply.github.com>
2019-06-28 19:17:09 +0900

    Add files via upload

--- PasswordCheck_EN.ps1
+++ PasswordCheck_EN.ps1
@@ -1,4 +1,4 @@
-$pw =(Read-Host Please enter your password -AsSecureString)
+$pw =(Read-Host Please type your password -AsSecureString)
 $encpw = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pw)
 $decpw  = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($encpw)
 $str1 = (([string]::concat(([security.cryptography.SHA1]::create().computehash([text.encoding]::ascii.getbytes($decpw))|%{ $_.tostring("x2")}))).ToUpper().Substring(0, 5))
@@ -6,7 +6,7 @@ $str2 = (([string]::concat(([security.cryptography.SHA1]::create().computehash([
 [System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 -bor [Net.SecurityProtocolType]::Tls11
 $api = (curl https://api.pwnedpasswords.com/range/$str1)
 if($api | select-string -CaseSensitive $str2 ){
-	echo "`r`n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`r`n            Password  leak`r`n   Please change your password immediately`r`n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`r`n"
+	echo "`r`n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`r`n      Leaked you are password ;X;X`r`n            Need to change `r`n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`r`n"
 }else{
 	echo "`r`nNot leaked`r`n"
 }
